794797c65b44871eadea44cd0ff61205f55d08f8
SophieARG <62552868+SophieARG@users.noreply.github.com>
2022-10-02 00:28:11 +0800

    Delete 第0次上机 directory

--- 第0次上机/2.py
+++
@@ -1,2 +0,0 @@
-n, x, y = map(int, input().split())
-print(n - y//x - (0 if y%x==0 else 1))
--- 第0次上机/4.py
+++
@@ -1,9 +0,0 @@
-a = int(input())
-if a % 400 == 0:
-    print('Y')
-elif a % 100 == 0:
-    print('N')
-elif a % 4 == 0:
-    print('Y')
-else:
-    print('N')
