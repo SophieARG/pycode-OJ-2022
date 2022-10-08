c20042815c717426a65fba4eaa5e060a5e16b846
SophieARG <62552868+SophieARG@users.noreply.github.com>
2022-10-09 02:14:54 +0800

    Update getHW.py

--- 作业统计/getHW.py
+++ 作业统计/getHW.py
@@ -1,35 +1,22 @@
-import requests, time
-from bs4 import BeautifulSoup
+import time
+from myOJ import OJparser
+from myCSV import CSVmaker
 
-MAXTRY = 100
+ojparser = OJparser()
+
 GROUPNAME = 'phyic'
 TITLE = '2022b01'
 
 now = time.strftime('%Y%m%d%H%M%S')
-log = open('log%s.txt' % now, 'w')
-res = open('res%s.txt' % now, 'w')
+suffix = '-%s-%s-%s.txt' % (GROUPNAME, TITLE, now)
 
-headers = {
-	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 Edg/97.0.1072.55"
-	}
+logfile = open('log' + suffix, 'w')
+datafile = open('data' + suffix, 'w')
 
-page = 0
-while page < MAXTRY:
-    page += 1
-    url = f"http://{GROUPNAME}.openjudge.cn/{TITLE}/ranking/?page={page}"
-    resp = requests.get(url, headers=headers)
-    bs = BeautifulSoup(resp.text, 'lxml')
-    log.write(bs.prettify())
-    for tr in bs.find_all('tr')[1:]:
-        tddata = []
-        for td in tr.find_all(name='td'):
-            _td = td.text.strip()
-            tddata.append(_td if _td else 'None')
-        res.write(' '.join(tddata) + '\n')
-    if bs.find(name='a', class_='nextprev', rel='next') is None:
-        page = MAXTRY
-    resp.close()
-    time.sleep(0.5)
+ojparser.parse(GROUPNAME, TITLE, logfile, datafile)
 
-log.close()
-res.close()
+logfile.close()
+datafile.close()
+
+csvmaker = CSVmaker()
+csvmaker.update('data' + suffix)
