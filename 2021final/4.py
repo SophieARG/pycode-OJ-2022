f114f1e5e50c9de3292b1f08c529482a0fa8a121
SophieARG <62552868+SophieARG@users.noreply.github.com>
2022-11-03 16:35:57 +0800

    Update 4.py

--- 2021final/4.py
+++ 2021final/4.py
@@ -1,4 +1,4 @@
 m = int(input().split()[-1])
 ans = input().split()
 for i in range(m):
-    print(sum(map(lambda x: x[0] == x[1], zip(input().split(), ans))))
+    print(sum(map(lambda x, y: x == y, input().split(), ans)))
